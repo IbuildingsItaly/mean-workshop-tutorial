605cd24c4c8d6fc3ceb25711299692f7abea9267
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-06-19 23:52:40 +0200

    added home/plan deps

---
+++ app/app/scripts/controllers/home.js
@@ -0,0 +1,28 @@
+angular
+    .module('meanApp')
+    .controller('HomeCtrl', ['$scope', '$location', '$http', 'Session', 'Plan', function ($scope, $location, $http, Session, Plan) {
+        $scope.addPlan = function (title) {
+            var plan = new Plan({title: title});
+
+            plan.$save(function (plan) {
+                if (plan.$resolved) {
+                    $location.path('/plan/' + plan.id);
+                }
+            });
+        };
+
+        $scope.removePlan = function (planId) {
+            var plan = new Plan({id: planId});
+
+            plan.$delete(function (plan) {
+                if (plan.$resolved) {
+                    delete $scope.plans[planId];
+                }
+            });
+        };
+
+        $http.get('/api/plans')
+            .success(function (plans) {
+                $scope.plans = plans;
+            });
+    }]);
