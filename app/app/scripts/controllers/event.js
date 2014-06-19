bad28feb8fb29cc349e054c3900624951ff1e1f4
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-06-20 00:35:04 +0200

    added event deps

---
+++ app/app/scripts/controllers/event.js
@@ -0,0 +1,64 @@
+angular
+    .module('meanApp')
+    .controller('EventCtrl', ['$scope', '$location', '$routeParams', 'Event', function ($scope, $location, $routeParams, Event) {
+        $scope.event = {
+            position: {
+                latitude: 44.70137621263722,
+                longitude: 10.63525390625
+            }
+        };
+        $scope.planId = $routeParams.planId;
+
+        $scope.map = {
+            center: {
+                latitude: 44.70137621263722,
+                longitude: 10.63525390625
+            },
+            zoom: 8
+        };
+
+        if ($routeParams.eventId) {
+            var event = new Event({planId: $routeParams.planId, eventId: $routeParams.eventId});
+
+            event.$get(function (event) {
+                $scope.event = event;
+                $scope.map.center.latitude = event.position.latitude;
+                $scope.map.center.longitude = event.position.longitude;
+            });
+        }
+
+        $scope.marker = {
+            options: {
+                draggable: true
+            },
+            events: {
+                dragend: function (evt) {
+                    $scope.event.position = {
+                        latitude: evt.position.lat(),
+                        longitude: evt.position.lng()
+                    };
+                }
+            }
+        };
+
+        $scope.saveEvent = function (event) {
+            if (event.eventId) {
+                event.$update(function (event) {
+                    if (event.$resolved) {
+                        $location.path('/plan/' + $scope.planId);
+                    }
+                });
+            }
+            else {
+                event = new Event(event);
+
+                event.planId = $scope.planId;
+
+                event.$save(function (event) {
+                    if (event.$resolved) {
+                        $location.path('/plan/' + $scope.planId);
+                    }
+                });
+            }
+        };
+    }]);
