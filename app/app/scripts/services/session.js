d2553b6ade4a6c0aef98e75a8efdb41aa722bffd
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-06-20 16:28:58 +0200

    added setHttpHeaders method

--- app/app/scripts/services/session.js
+++ app/app/scripts/services/session.js
@@ -3,6 +3,12 @@ angular
     .service('Session', ['$cookieStore', '$http', function ($cookieStore, $http) {
         var me = this;
 
+        me.setHttpHeaders = function () {
+            var token = $cookieStore.get('token');
+
+            if (token && token.length > 0 && !$http.defaults.headers.common.Authorization) $http.defaults.headers.common.Authorization = token;
+        };
+
         me.authenticate = function (token) {
             $cookieStore.put('token', token);
             $http.defaults.headers.common.Authorization = token;
