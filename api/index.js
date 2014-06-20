acebc66d05fcb2a19b7e40a8b961995ca02f8165
Alessio Barnini <alessio@ibuildings.it>
2014-06-20 09:18:10 +0200

    Create Express

---
+++ api/index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+    , bodyParser = require('body-parser')
+    , app = express()
+    , logger = require("./logger")
+    , port = process.env.PORT || 12345 // set our port
+    , cors = require('cors');
+
+corsOptions = {
+    origin: '*',
+    methods: 'POST,GET,PUT,DELETE',
+    allowedHeaders: 'X-Requested-With,Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
+
+var router = express.Router();
+router.use(function (req, res, next) {
+    logger.info('%s %s %s', req.method, req.url, req.path);
+    next(); //!important
+});
+
+app.use(bodyParser());
+
+// all requests to this router will first hit this middleware
+app.use('/api/', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+logger.info('Server Listening at ' + port + ' port');
+
+
